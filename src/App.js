62c4f24e57ec7703755ba4fd2de753441df6af78
Tizian <tizianappler@yahoo.de>
2021-12-24 13:23:45 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+
+function Item(props) {
+    return (
+        <div className={"Item"} itemID={props}>
+            <h2>{props}</h2>
+            <div className={"ItemActions"}>
+                <input type={"button"} value={"Add"} onClick={() => {
+                    console.log(props)
+                    addItemCount(props.items, props.setItems)
+                    return null
+                }}/>
+                <input type={"button"} value={"Remove"} onClick={() => {
+                    return null
+                }}/>
+            </div>
+        </div>
+    );
+}
+
+function addItemCount(items, setItems) {
+    return null
+}
+
+function removeItem(items, setItem) {
+
+}
+
+function Items(props) {
+    console.log(props.items)
+    if (Array.isArray(props.items)) {
+        return (props.items.map(Item))
+    }
+    return null
+}
+
+const addItem = (newItem, items, setItems) => {
+    console.log(newItem)
+    if (newItem.length !== 0 && newItem !== "" && !(newItem in items)) {
+        setItems([...items, newItem])
+    }
+}
+
+function clickHandler(itemInput, items, setItems) {
+    addItem(itemInput.current.value, items, setItems)
+    itemInput.current.value = "";
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let itemInput = React.createRef()
+    const [items, setItems] = useState([])
+
+    const getItems = () => items;
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Einkaufsliste</h1>
+                <div className={"Outer"}>
+                    <div className={"Inner"}>
+                        <Items items={items} setItems={setItems}/>
+                    </div>
+                </div>
+                <div>
+                    <input type={"textarea"} ref={itemInput} onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                            clickHandler(itemInput, items, setItems)
+                        }
+                        return null;
+                    }}/>
+                    <input type={"button"} value={"Senden"} onClick={() => clickHandler(itemInput, items, setItems)}/>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
